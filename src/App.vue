fccd2936ee4546ee5d8dae175ea14664f90d3dc7
artemShvalev <60879559+artemShvalev@users.noreply.github.com>
2020-12-04 18:43:44 +0500

    commit 2
    Add component Users

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
+    <EnterUsers/>
   </div>
 </template>
 
 <script>
+import EnterUsers from './components/EnterUsers.vue';
 
 export default {
   name: 'App',
+  components: { EnterUsers },
 };
 </script>
 
---
+++ src/components/EnterUsers.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="container">
+      <div id="app" class="form">
+        <label>Имя</label>
+        <input type="text" v-model="firstName" placeholder=" имя"/><br>
+        <label>Фамилия</label>
+        <input type="text" v-model="lastName" placeholder=" фамилия"/><br>
+        <label>Возраст</label>
+        <input type="number" v-model="age" min="18" required placeholder=" возраст"/><br>
+        <label>Дата регистрации</label><br>
+        <input type="date" v-model="date" placeholder="дата"/>
+        <button  class="btn" @click="addUser()">
+          Добавить пользователя
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EnterUsers',
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      age: '',
+      date: '',
+    };
+  },
+  methods: {
+    addUser(firstName, lastName, age, date) {
+      firstName = this.addUser;
+      lastName = this.addUser;
+      age = this.addUser;
+      date = this.addUser;
+    },
+  },
+};
+</script>
+
+<style scoped lang=scss>
+
+</style>
